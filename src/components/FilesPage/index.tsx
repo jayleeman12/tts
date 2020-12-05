870e6d0ce4cc257fa0429ae5d484c5d3d5b74223
jay leeman <jayleeman12@gmail.com>
2020-12-05 12:07:38 +0200

    Add file icon to filespage

--- src/components/FilesPage/index.tsx
+++ src/components/FilesPage/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Dimensions, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'
 import { human } from 'react-native-typography';
 import { COLORS } from '../../globals';
 import { File } from '../../types';
@@ -11,18 +12,24 @@ const files: File[] = [
 ]
 
 const FilesPage = () => (
-    <View style={{ display: 'flex' }}>
-        <View style={{ backgroundColor: COLORS.primary.dark, height: '15%', justifyContent: 'center' }}>
+    <View style={{ display: 'flex', height: '100%' }}>
+        <View style={{ backgroundColor: COLORS.primary.dark, height: '10%', justifyContent: 'center' }}>
             <Text style={[human.title1, { color: 'white', marginLeft: '3%' }]}>Files</Text>
         </View>
-        {files.map((file: File, index: number) => (
-            <View key={index} style={{
-                height: '17%',
-                justifyContent: 'center'
-            }}>
-                <Text style={[human.body, { 'marginLeft': '3%' }]}>{file.name}</Text>
-            </View>
-        ))}
+        <View style={{height: '100%'}}>
+            {files.map((file: File, index: number) => (
+                <View key={index} style={{
+                    marginLeft: '4%',
+                    justifyContent: 'flex-start',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    height: '12%'
+                }}>
+                    <Icon name='file' size={25} color={COLORS.secondary.light} />
+                    <Text style={[human.body, { 'marginLeft': '3%' }]}>{file.name}</Text>
+                </View>
+            ))}
+        </View>
     </View>
 );
 
