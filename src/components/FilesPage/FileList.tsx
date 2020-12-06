375a67f7709618d37d5af5fc0355d7668a5432e0
jay leeman <jayleeman12@gmail.com>
2020-12-07 00:34:18 +0200

    Make the rows clickable
    This requires further work, to make the touchable take up the entire row height

--- src/components/FilesPage/FileList.tsx
+++ src/components/FilesPage/FileList.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Dimensions, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
+import { Dimensions, ListRenderItemInfo, Text, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback, View } from 'react-native'
 import { SwipeListView } from 'react-native-swipe-list-view'
 import { human } from 'react-native-typography'
 import Icon from 'react-native-vector-icons/FontAwesome'
@@ -9,44 +9,56 @@ import { File } from '../../types'
 export type FileListProps = {
     files: File[]
     onFileDeleted: (file: File) => void;
+    onFilePressed: (file: File) => void;
 }
 
-const FileList: React.FunctionComponent<FileListProps> = props => (
-    <SwipeListView
-        contentContainerStyle={{ flexGrow: 1, marginTop: '8%', marginLeft: '3%' }}
-        swipeRowStyle={{ height: '12%' }}
-        data={props.files}
-        keyExtractor={file => file.path}
-        renderItem={data => (
-            <View style={{
-                justifyContent: 'flex-start',
-                flexDirection: 'row',
-                alignItems: 'center',
-                backgroundColor: 'white'
-            }}>
-                <Icon name='file' size={25} color={COLORS.secondary.light} />
-                <Text style={[human.body, { 'marginLeft': '3%' }]}>{data.item.name}</Text>
-            </View>
-        )}
-        renderHiddenItem={data => (
-            <View style={{
-                flex: 1,
-                flexDirection: 'row',
-                justifyContent: 'flex-end',
-            }}>
-                <TouchableOpacity
-                    onPress={() => props.onFileDeleted(data.item)}
-                    style={{ width: '15%', justifyContent: 'center' }}
-                >
-                    <Icon name='trash' size={25} color={'red'} />
-                </TouchableOpacity>
-            </View>
-        )}
-        disableRightSwipe={true}
-        closeOnRowBeginSwipe={true}
-        closeOnRowPress={true}
-        rightOpenValue={-Dimensions.get('window').width * 0.15}
-    />
-);
+const FileList: React.FunctionComponent<FileListProps> = props => {
+    const [isRowClosed, setRowClosed] = useState<boolean>(false);
+    return (
+        <SwipeListView
+            contentContainerStyle={{ flexGrow: 1, marginTop: '8%', marginLeft: '3%' }}
+            swipeRowStyle={{ height: '12%' }}
+            data={props.files}
+            keyExtractor={file => file.path}
+            renderItem={data => (
+                <TouchableHighlight onPress={() => {
+                    if (isRowClosed) {
+                        console.log('fuck this');
+                    }
+                }} style={{}}>
+                    <View style={{
+                        justifyContent: 'flex-start',
+                        flexDirection: 'row',
+                        alignItems: 'center',
+                        backgroundColor: 'white'
+                    }}>
+                        <Icon name='file' size={25} color={COLORS.secondary.light} />
+                        <Text style={[human.body, { 'marginLeft': '3%' }]}>{data.item.name}</Text>
+                    </View>
+                </TouchableHighlight>
+            )}
+            renderHiddenItem={data => (
+                <View style={{
+                    flex: 1,
+                    flexDirection: 'row',
+                    justifyContent: 'flex-end',
+                }}>
+                    <TouchableOpacity
+                        onPress={() => props.onFileDeleted(data.item)}
+                        style={{ width: '15%', justifyContent: 'center' }}
+                    >
+                        <Icon name='trash' size={25} color={'red'} />
+                    </TouchableOpacity>
+                </View>
+            )}
+            disableRightSwipe={true}
+            closeOnRowBeginSwipe={true}
+            closeOnRowPress={true}
+            rightOpenValue={- Dimensions.get('window').width * 0.15}
+            onRowDidOpen={() => setRowClosed(false)}
+            onRowDidClose={() => setRowClosed(true)}
+        />
+    );
+}
 
 export default FileList;
